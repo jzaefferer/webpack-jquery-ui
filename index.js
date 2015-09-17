4967da58346a9c90d8d85cca4ad983780b6b63ac
Jörn Zaefferer <joern.zaefferer@gmail.com>
2015-09-17 22:36:13 +0200

    Use oldschool node require instead of ES6 modules to keep it simple

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-import './main.css';
-import 'jquery-ui/themes/base/core.css';
-import 'jquery-ui/themes/base/menu.css';
-import 'jquery-ui/themes/base/autocomplete.css';
-import 'jquery-ui/themes/base/theme.css';
-import $ from 'jquery';
-import autocomplete from 'jquery-ui/ui/widgets/autocomplete';
+require('./main.css');
+require('jquery-ui/themes/base/core.css');
+require('jquery-ui/themes/base/menu.css');
+require('jquery-ui/themes/base/autocomplete.css');
+require('jquery-ui/themes/base/theme.css');
+var $ = require('jquery');
+var autocomplete = require('jquery-ui/ui/widgets/autocomplete');
 
 $('<h1>Welcome to the programming languages quiz</h1>').appendTo('body');
 new autocomplete({
--- webpack-config.js
+++ webpack-config.js
@@ -16,11 +16,6 @@ module.exports = {
 	module: {
 		loaders: [
 			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				loader: 'babel'
-			},
-			{
 				test: /\.css$/,
 				loader: ExtractTextPlugin.extract("style-loader", "css-loader")
 			},
