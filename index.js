af2ad0dbd797f300ca5f995d9c29010560482297
Jörn Zaefferer <joern.zaefferer@gmail.com>
2016-11-20 11:44:11 +0100

    Experimental datepicker example

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ require('./main.css');
 require('jquery-ui/themes/base/core.css');
 require('jquery-ui/themes/base/menu.css');
 require('jquery-ui/themes/base/autocomplete.css');
+require('jquery-ui/themes/base/calendar.css');
+require('jquery-ui/themes/base/datepicker.css');
 require('jquery-ui/themes/base/theme.css');
 var $ = require('jquery');
-var autocomplete = require('jquery-ui/ui/widgets/autocomplete');
+var Autocomplete = require('jquery-ui/ui/widgets/autocomplete');
+var Datepicker = require('jquery-ui/ui/widgets/datepicker');
 
 $('<h1>Welcome to the programming languages quiz</h1>').appendTo('body');
-new autocomplete({
+new Autocomplete({
   source: ['javascript', 'css', 'c', 'objectivec']
 }).element.appendTo('body').focus();
+
+$('<h2>When do you want to start the quiz?</h2>').appendTo('body');
+new Datepicker({}).element.appendTo('body');
--- webpack-config.js
+++ webpack-config.js
@@ -1,6 +1,7 @@
 var Clean = require('clean-webpack-plugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var GlobalizePlugin = require('globalize-webpack-plugin');
 
 module.exports = {
 	entry: {
@@ -25,6 +26,13 @@ module.exports = {
 	plugins: [
 		new Clean(['dist']),
 		new ExtractTextPlugin("app.[hash].css"),
+		new GlobalizePlugin({
+			// toggle this for actual production builds
+			production: false,
+			developmentLocale: "de",
+			supportedLocales: [ "de" ],
+			output: "globalize-compiled-data-[locale].[hash].js"
+		}),
 		new HtmlWebpackPlugin({
 			title: 'jQuery UI Autocomplete demo, built with webpack'
 		})
